8e2b8cd04a76199e4dce75d2a7ed696181365841
austin <austinhuang1203@gmail.com>
2022-09-03 15:34:44 +0800

    added cancel button

--- code.js
+++ code.js
@@ -6,4 +6,16 @@ const btn = document.getElementById("btn");
 btn1.addEventListener("click",)
 */
 const btn1 = document.getElementById("btn1");
-btn1.addEventListener("click",)
+btn1.addEventListener("click",cancel);
+
+//Get elements
+const display2 = document.getElementById("display2");
+const display3 = document.getElementById("display3");
+
+
+//functions
+
+function cancel(){
+    display2.innerHTML = "";
+    display3.innerHTML = "";
+}
