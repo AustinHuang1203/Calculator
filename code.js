127d93f79cde27575a1f4d406c02269ed0626082
austin <austinhuang1203@gmail.com>
2022-09-03 16:08:18 +0800

    added more button functions

--- code.js
+++ code.js
@@ -17,6 +17,48 @@ btn3.addEventListener("click",multiply);
 const btn4 = document.getElementById("btn4");
 btn4.addEventListener("click",delete1);
 
+const btn5 = document.getElementById("btn5");
+btn5.addEventListener("click",()=>{addnum(7)});
+
+const btn6 = document.getElementById("btn6");
+btn6.addEventListener("click",()=>{addnum(8)});
+
+const btn7 = document.getElementById("btn7");
+btn7.addEventListener("click",()=>{addnum(9)});
+
+const btn8 = document.getElementById("btn8");
+btn8.addEventListener("click",minus);
+
+const btn9 = document.getElementById("btn9");
+btn9.addEventListener("click",()=>{addnum(4)});
+
+const btn10 = document.getElementById("btn10");
+btn10.addEventListener("click",()=>{addnum(5)});
+
+const btn11 = document.getElementById("btn11");
+btn11.addEventListener("click",()=>{addnum(6)});
+
+const btn12 = document.getElementById("btn12");
+btn12.addEventListener("click",add);
+
+const btn13 = document.getElementById("btn13");
+btn13.addEventListener("click",()=>{addnum(1)});
+
+const btn14 = document.getElementById("btn14");
+btn14.addEventListener("click",()=>{addnum(2)});
+
+const btn15 = document.getElementById("btn15");
+btn15.addEventListener("click",()=>{addnum(3)});
+
+const btn16 = document.getElementById("btn16");
+btn16.addEventListener("click",modulo);
+
+const btn17 = document.getElementById("btn17");
+btn17.addEventListener("click",()=>{addnum(0)});
+
+const btn18 = document.getElementById("btn18");
+btn18.addEventListener("click",decimal);
+
 
 
 //Display
@@ -51,4 +93,29 @@ function multiply(){
 function delete1(){
     calc.slice(0,-1);
     display2.innerHTML = display2.innerHTML.slice(0,-1);
+}
+
+function addnum(x){
+    calc = calc + "x";
+    display2.innerHTML += x;
+}
+
+function minus(){
+    calc = calc + "-";
+    display2.innerHTML += "-";
+}
+
+function add(){
+    calc = calc + "+";
+    display2.innerHTML += "+";
+}
+
+function modulo(){
+    calc = calc + "%";
+    display2.innerHTML += "%";
+}
+
+function decimal(){
+    calc = calc + ".";
+    display2.innerHTML += ".";
 }
