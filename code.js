95975efd8b17351d628af2444a87ed42073f9860
austin <austinhuang1203@gmail.com>
2022-09-03 15:14:54 +0800

    added css

---
+++ code.js
@@ -0,0 +1,9 @@
+
+
+// event listerners
+/*
+const btn = document.getElementById("btn");
+btn1.addEventListener("click",)
+*/
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click",)
